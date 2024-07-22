c9dcfe856c71b6a03e4e7c96d9c806a3440828c6
shrabanti1 <jharnamazumdar78623@gmail.com>
2024-07-22 22:03:25 +0530

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -49,7 +49,7 @@ const CartPage = () => {
   const getToken = async () => {
     try {
       const { data } = await axios.get(
-        "https://shopnow-5f1x.onrender.com:8080/api/v1/product/braintree/token"
+        "https://shopnow-5f1x.onrender.com/api/v1/product/braintree/token"
       );
       setClientToken(data?.clientToken);
     } catch (error) {
@@ -66,7 +66,7 @@ const CartPage = () => {
       setLoading(true);
       const { nonce } = await instance.requestPaymentMethod();
       const { data } = await axios.post(
-        "https://shopnow-5f1x.onrender.com:8080/api/v1/product/braintree/payment",
+        "https://shopnow-5f1x.onrender.com/api/v1/product/braintree/payment",
         {
           nonce,
           cart,
@@ -108,7 +108,7 @@ const CartPage = () => {
                 <div className="row card flex-row" key={p._id}>
                   <div className="col-md-4">
                     <img
-                      src={`https://shopnow-5f1x.onrender.com:8080/api/v1/product/product-photo/${p._id}`}
+                      src={`https://shopnow-5f1x.onrender.com/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="100%"
