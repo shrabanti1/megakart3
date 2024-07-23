e43abe9032c629924c57f39bfd7a07731ccd6bd6
shrabanti1 <jharnamazumdar78623@gmail.com>
2024-07-23 10:49:15 +0530

    Update UpdateProduct.js

--- src/pages/Admin/UpdateProduct.js
+++ src/pages/Admin/UpdateProduct.js
@@ -24,7 +24,7 @@ const UpdateProduct = () => {
   const getSingleProduct = async () => {
     try {
       const { data } = await axios.get(
-        `https://shopnow-5f1x.onrender.com/api/v1/product/get-product/${params.slug}`
+        `https://fullstackbackend-1-v8m0.onrender.com/api/v1/product/get-product/${params.slug}`
       );
       setName(data.product.name);
       setId(data.product._id);
@@ -46,14 +46,14 @@ const UpdateProduct = () => {
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(
-        "https://shopnow-5f1x.onrender.com/api/v1/category/get-category"
+        "https://fullstackbackend-1-v8m0.onrender.com/api/v1/category/get-category"
       );
       if (data?.success) {
         setCategories(data?.category);
       }
     } catch (error) {
       console.log(error);
-      toast.error("Something wwent wrong in getting catgeory");
+      toast.error("Something went wrong in getting catgeory");
     }
   };
 
@@ -73,7 +73,7 @@ const UpdateProduct = () => {
       photo && productData.append("photo", photo);
       productData.append("category", category);
       const { data } = axios.put(
-        `https://shopnow-5f1x.onrender.com/api/v1/product/update-product/${id}`,
+        `https://fullstackbackend-1-v8m0.onrender.com/api/v1/product/update-product/${id}`,
         productData
       );
       if (data?.success) {
@@ -94,7 +94,7 @@ const UpdateProduct = () => {
       let answer = window.prompt("Are You Sure want to delete this product ? ");
       if (!answer) return;
       const { data } = await axios.delete(
-        `https://shopnow-5f1x.onrender.com/api/v1/product/delete-product/${id}`
+        `https://fullstackbackend-1-v8m0.onrender.com/api/v1/product/delete-product/${id}`
       );
       toast.success("Product DEleted Succfully");
       navigate("/dashboard/admin/products");
@@ -155,7 +155,7 @@ const UpdateProduct = () => {
                 ) : (
                   <div className="text-center">
                     <img
-                      src={`https://shopnow-5f1x.onrender.com/api/v1/product/product-photo/${id}`}
+                      src={`https://fullstackbackend-1-v8m0.onrender.com/api/v1/product/product-photo/${id}`}
                       alt="product_photo"
                       height={"200px"}
                       className="img img-responsive"
